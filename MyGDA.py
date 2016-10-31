35abe422def585b60b836b70dd869362e32eba70
JZ <zane.jiang@gmail.com>
2016-10-30 22:23:02 -0700

    Feat 1) Make the learning rate as the parameter of fit function        2) Add the Stochatic Gradiescent Algorithm
    Changes to be committed:
    	modified:   MyGDA.py

--- MyGDA.py
+++ MyGDA.py
@@ -7,8 +7,7 @@ class GDAlgorithm(object):
 	"""
 	The frame of gradient descent algorithm for linear model
 	"""
-	def __init__(self,alpha,X,Y,theta):
-		self.alpha = alpha
+	def __init__(self,X,Y,theta):
 		self.X = X
 		self.Y = Y
 		self.theta = theta
@@ -43,42 +42,56 @@ class GDAlgorithm(object):
 		return X[p,:],Y[p]
 
 
-	def fit(self,num_of_iteration=100, eps=0.001, alpha=1, algorithm='batch',batch_size=-1):
+	def SGD(self, num_of_iteration=100, eps=0.001,alpha=1):
+		"""Calculate theta by Stochastic Gradient Descent"""
+		self.J=[0]
+
+		actual_iteration=0
+		for i in range(num_of_iteration):
+			X,Y=self.shuffle(self.X,self.Y)
+			theta = self.theta
+			actual_iteration=actual_iteration+1
+
+			for j in range(len(Y)):
+				Xt=X[j,:].flatten()
+				Yt=Y[j]
+				loss = Yt-self.h_theta(Xt)
+				gradient = loss*Xt
+				self.theta = self.theta+alpha*gradient
+
+			self.J.append(self.J_theta(X,Y))
+			if(np.linalg.norm(self.theta-theta)<eps):
+				break
+
+		return actual_iteration
+
+	def fit(self,num_of_iteration=100, eps=0.001, alpha=1, algorithm='batch'):
 		"""Calculate theta by gradient descent"""
 		self.J=[0]
 		if algorithm=='batch':
 			batch_size=len(self.Y)
+		else:
+			return self.SGD(num_of_iteration,eps,alpha)
 
+		actual_iteration=0
 		for i in range(num_of_iteration):
-			Y=self.Y
-			X=self.X
-			if algorithm=='Stochatic':
-				X,Y = self.shuffle(self.X,self.Y)
-
-			for j in np.arange(0,len(Y),batch_size):
-				#print j,
-				Xt= X[j:j+batch_size,:]
-				Yt= Y[j:j+batch_size]
-				#print Xt
-				loss = Yt-self.h_theta(Xt)
-				gradient = np.dot(Xt.T,loss)/(len(Yt))
-				self.theta = self.theta+alpha*gradient
-				#print self.theta
+			theta = self.theta
+			loss = self.Y-self.h_theta(self.X)
+			gradient = np.dot(Xt.T,loss)/(len(Yt))
+			self.theta = self.theta+alpha*gradient
 			self.J.append(self.J_theta(X,Y))
 
-			if abs(self.J[-1]-self.J[-2])<eps:
-				print "!Convengence gotten"
+			if np.linalg.norm(self.theta-theta)<eps:
 				break
-		return
+
+		return actual_iteration
 			
 class LMS(GDAlgorithm):
-	def __init__(self,alpha,X,Y):
+	def __init__(self,X,Y):
 		(m,n)=X.shape
 		X_=np.hstack((np.ones(m).reshape((m,1)),X))
-		#print X_.shape
-		theta = np.random.rand(n+1)
-		#theta = np.zeros(n+1)
-		GDAlgorithm.__init__(self,alpha,X_,Y,theta)
+		theta = np.zeros(n+1)
+		GDAlgorithm.__init__(self,X=X_,Y=Y,theta=theta)
 		
 	def J_theta(self,X,Y):
 		m = Y.size
@@ -111,6 +124,20 @@ class LogisticRegression_Binary(LMS):
 		h=self.h_theta(X)
 		Y_=np.ones_like(Y)-Y
 		h_=np.ones_like(Y)-self.h_theta(X)
-		logL=np.dot(Y.T,np.log(h))+np.dot(Y_.T,np.log(h_))
-		#print logL
+		logL=1.0/Y.shape[0]*(np.dot(Y.T,np.log(h))+np.dot(Y_.T,np.log(h_)))
 		return logL
+
+class SoftmaxRegression(LMS):
+	def h_theta(self,X):
+		Z=np.dot(X,self.theta)
+		a=1.0/(1.0+np.exp(-1.0*Z))
+		return
+
+	def __init__(self,X,Y):
+		(m, n) = X.shape
+		X_ = np.hstack((np.ones(m).reshape((m, 1)), X))
+		#number of classes
+		Num_of_Classes = len(set(Y.tolist()))
+		theta=np.zeros((n+1,Num_of_Classes),dtype=np.float)
+		# print X_.shape
+		GDAlgorithm.__init__(self,X_, Y, theta)
