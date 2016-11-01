2956765a6c0f3cd4618634b4cb2e63c9f3c3355f
JZ <zane.jiang@gmail.com>
2016-11-01 15:13:25 -0700

    Feat: Complete SoftmaxRegression Refactor: Seperate th gradient function from fit and SGD
     On branch softmax
     Changes to be committed:
    	modified:   MyGDA.py

--- MyGDA.py
+++ MyGDA.py
@@ -37,10 +37,21 @@ class GDAlgorithm(object):
 		ax.plot(self.J)
 		fig.show()
 	
+	def hot1(self,X,number_of_class):
+		m=1
+		if type(X) is np.ndarray:
+			m = len(X)
+
+		_hot1=np.zeros(shape=(m,number_of_class),dtype=np.float)
+		_hot1[np.arange(m),X]=1.0
+		return _hot1
+
 	def shuffle(self,X,Y):
 		p=np.random.permutation(len(Y))
 		return X[p,:],Y[p]
 
+	def indicator(self,X,k):
+		return (X==k).astype(np.float)
 
 	def SGD(self, num_of_iteration=100, eps=0.001,alpha=1):
 		"""Calculate theta by Stochastic Gradient Descent"""
@@ -55,11 +66,10 @@ class GDAlgorithm(object):
 			for j in range(len(Y)):
 				Xt=X[j,:].flatten()
 				Yt=Y[j]
-				loss = Yt-self.h_theta(Xt)
-				gradient = loss*Xt
+				gradient = self.gradient(Xt,Yt,algorithm='Stochatic')
 				self.theta = self.theta+alpha*gradient
 
-			self.J.append(self.J_theta(X,Y))
+			#self.J.append(self.J_theta(X,Y))
 			if(np.linalg.norm(self.theta-theta)<eps):
 				break
 
@@ -76,16 +86,26 @@ class GDAlgorithm(object):
 		actual_iteration=0
 		for i in range(num_of_iteration):
 			theta = self.theta
-			loss = self.Y-self.h_theta(self.X)
-			gradient = np.dot(self.X.T,loss)/(len(self.Y))
+			gradient = self.gradient(self.X,self.Y)
+			#print theta,gradient
 			self.theta = self.theta+alpha*gradient
-			self.J.append(self.J_theta(self.X,self.Y))
+			#self.J.append(self.J_theta(self.X,self.Y))
 
 			if np.linalg.norm(self.theta-theta)<eps:
 				break
 
 		return actual_iteration
-			
+
+	def gradient(self,X,Y,algorithm='batch'):
+		if(algorithm=='batch'):
+			loss = Y - self.h_theta(X)
+			gradient = np.dot(X.T, loss) / (len(Y))
+		else:
+			loss = Y - self.h_theta(X)
+			gradient = loss*X
+		return gradient
+
+
 class LMS(GDAlgorithm):
 	def __init__(self,X,Y):
 		(m,n)=X.shape
@@ -128,16 +148,51 @@ class LogisticRegression_Binary(LMS):
 		return logL
 
 class SoftmaxRegression(LMS):
+	Num_of_Classes=0
+	def J_theta(selfself,X,Y):
+		pass
+
+	def score(self,X,y):
+		ypredicated = self.predicate(X)
+		return np.sum(ypredicated == y) * 1.0 / len(y)
+
+	def predicate(self,X):
+		(m, n) = X.shape
+		X_ = np.hstack((np.ones(m).reshape((m, 1)), X))
+		pik=self.h_theta(X_)
+		#pik is (m,k)
+		return np.argmax(pik,axis=1)
+
+	def gradient(self,X,Y,algorithm='batch'):
+		if(algorithm=='Stochatic'):
+			X=X[:,np.newaxis].T
+		Y_hot1= self.hot1(Y,number_of_class=self.Num_of_Classes)
+		#Y_hot1 is matrix of (m,k)
+		pik=Y_hot1-self.h_theta(X)
+		#pik is matrix of (m,k) representing the progability of each x sample classified to class 1,....K
+		#x is matrix of (m,n+1)
+		gradient=[]
+		for pk in pik.T:
+			#pk is a vector of k components
+			gradient.append((X*pk[:,np.newaxis]).mean(axis=0).tolist())
+		#print gradient
+		#the gradient will be (n+1,k)
+		return np.array(gradient).T
+
+
 	def h_theta(self,X):
-		Z=np.dot(X,self.theta)
-		a=1.0/(1.0+np.exp(-1.0*Z))
-		return
+		#X is the matrix of (m,n+1)
+		s = np.dot(X,self.theta)
+		#now s is (m,k)
+		exps= np.exp(s)
+		return exps/(np.sum(exps,axis=1)[:,np.newaxis])
 
 	def __init__(self,X,Y):
 		(m, n) = X.shape
 		X_ = np.hstack((np.ones(m).reshape((m, 1)), X))
 		#number of classes
-		Num_of_Classes = len(set(Y.tolist()))
-		theta=np.zeros((n+1,Num_of_Classes),dtype=np.float)
-		# print X_.shape
+		self.Num_of_Classes = len(np.unique(Y))
+		#here our theta is a matrix of (n+1,K)
+		theta=np.zeros((n+1,self.Num_of_Classes),dtype=np.float)
+		print theta.shape,X_.shape
 		GDAlgorithm.__init__(self,X_, Y, theta)
